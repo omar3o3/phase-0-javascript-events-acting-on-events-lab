ecd5aac8f5021bc426de2d480af351dfd4ce16d3
Omar <otantawy53@gmail.com>
2022-05-02 13:34:10 -0400

    commit tree 50bf8c12ceb01047eb36ae1baab2c2e5cd49bc63

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+const dodger = document.getElementById('dodger');
+
+dodger.style.backgroundColor = '#FF69B4';
+
+dodger.style.bottom = '0px'
+
+// dodger.style.left = '0px'
+
+// document.addEventListener('keydown', function(e){
+//     if (e.key === 'ArrowLeft'){
+//         const leftNumbers = dodger.style.left.replace('px' , '');
+//         const left = parseInt(leftNumbers , 10);
+
+//         dodger.style.left = `${left - 1}px`;
+//     }
+// })
+
+dodger.addEventListener('keydown' , function(e){
+    if (e === 'ArrowLeft'){
+        moveDodgerLeft();
+    } else if (e === 'ArrowRight'){
+        moveDodgerRight();
+    }
+})
+
+function moveDodgerLeft(e){
+    const leftNumbers = dodger.style.left.replace('px' , '');
+    const left = parseInt(leftNumbers , 10);
+
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(e){
+    // const rightNumbers = dodger.style.left.replace('px' , '');
+    // const right = parseInt(rightNumbers , 10);
+    const leftNumbers = dodger.style.left.replace('px' , '');
+    const left = parseInt(leftNumbers , 10);
+
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
